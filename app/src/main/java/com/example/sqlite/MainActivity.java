c5f94ba6c2236da26d909ab742f43b681b98f7db
Softwarica College Lab <softwarica@softwarica.com>
2019-05-02 11:19:19 +0545

    INSERTING INTO TABLE

--- app/src/main/java/com/example/sqlite/MainActivity.java
+++ app/src/main/java/com/example/sqlite/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.sqlite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText etWord, etMeaning;
+    Button btnAddWord;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etWord = findViewById(R.id.etWord);
+        etMeaning = findViewById(R.id.etMeaning);
+        btnAddWord = findViewById(R.id.btnAddWord);
+
     }
 }
---
+++ app/src/main/java/helper/MyHelper.java
@@ -0,0 +1,39 @@
+package helper;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MyHelper extends SQLiteOpenHelper {
+
+    private static final String databaseName = "DictionayDB";
+
+    private static final int dbVersion = 1;
+
+    private static final String tblword = "tblword";
+    private static final String WordID = "wordId";
+    private static final String Word = "word";
+    private static final String Meaning = "Meaning";
+
+    public MyHelper(Context context) {
+        super(context, databaseName, null, dbVersion);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query = " CREATE TABLE " + tblword +
+                "("
+                + WordID + "INTEGER PRIMARY KEY AUTOINCREAMENT," +
+                Word + "TEXT,"
+                + Meaning + "TEXT" +
+                ")";
+        db.execSQL(query);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
